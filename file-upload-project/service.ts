d2517b350f68040c62a5f943d1fe42dc4f2c147c
ThuF <jordan.p.pavlov@gmail.com>
2023-12-26 18:58:43 +0200

    Updates sample

--- file-upload-project/service.ts
+++ file-upload-project/service.ts
@@ -1,22 +1,33 @@
 import { upload, request, response } from "@dirigible/http";
+import { cmis } from "@dirigible/cms";
+import { streams } from "@dirigible/io";
 
 if (request.getMethod() === "POST") {
     if (upload.isMultipartContent()) {
         const fileItems = upload.parseRequest();
         for (let i = 0; i < fileItems.size(); i++) {
             const fileItem = fileItems.get(i);
-            const contentType = fileItem.getContentType();
-            console.log(`Content Type: ${contentType}`);
-            console.log(`Filename: ${fileItem.getOriginalFilename()}`);
-            // console.log(`Text: ${fileItem.getText()}`);
 
-            response.setContentType(contentType);
-            response.write(fileItem.getBytesNative());
+            const fileName = fileItem.getName();
+            const contentType = fileItem.getContentType();
+            const bytes = fileItem.getBytes();
+
+            const inputStream = streams.createByteArrayInputStream(bytes);
+
+            const cmisSession = cmis.getSession();
+            const contentStream = cmisSession.getObjectFactory().createContentStream(fileName, bytes.length, contentType, inputStream);
+
+            cmisSession.createDocument("file-upload-project/uploads", {
+                [cmis.OBJECT_TYPE_ID]: cmis.OBJECT_TYPE_DOCUMENT,
+                [cmis.NAME]: fileName
+            }, contentStream, cmis.VERSIONING_STATE_MAJOR);
+
         }
+        response.sendRedirect("/services/web/ide-documents/");
     } else {
         response.println("The request's content must be 'multipart'");
     }
-} else if (request.getMethod() === "GET") {
+} else {
     response.println("Use POST request.");
 }
 
